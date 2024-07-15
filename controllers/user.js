1a03ea534d8494ccde71053d234df38de3eef36b
Yogesh07-pixel <yogeshrawat518@gmail.com>
2024-07-15 15:10:39 +0530

    updated

--- controllers/user.js
+++ controllers/user.js
@@ -19,9 +19,8 @@ async function handleUserLogin(req, res) {
       error: "Invalid username or Password",
     });
 
-  const sessionId = uuidv4();
-  setUser(sessionId, user);
-  res.cookie("uid", sessionId);
+  const token = setUser(user);
+  res.cookie("uid", token);
   return res.redirect("/");
 }
 
--- service/auth.js
+++ service/auth.js
@@ -1,11 +1,23 @@
-const sessionIdToUserMap = new Map();
+const jwt = require("jsonwebtoken");
+const secret = "Yogesh@9654$";
 
-function setUser(id, user) {
-  sessionIdToUserMap.set(id, user);
+function setUser(user) {
+  return jwt.sign(
+    {
+      _id: user._id,
+      email: user.email,
+    },
+    secret
+  );
 }
 
-function getUser(id) {
-  return sessionIdToUserMap.get(id);
+function getUser(token) {
+  if (!token) return null;
+  try {
+    return jwt.verify(token, secret);
+  } catch (error) {
+    return null;
+  }
 }
 
 module.exports = {
